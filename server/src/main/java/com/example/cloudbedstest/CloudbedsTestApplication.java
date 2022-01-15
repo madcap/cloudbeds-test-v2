05340994bcaae4abb6b83f8aaa8ba0af3152ff64
James Maatman <james.maatman@trane.com>
2022-01-15 15:59:35 -0600

    working grcp example server

---
+++ server/src/main/java/com/example/cloudbedstest/CloudbedsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudbedstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbedsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbedsTestApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/cloudbedstest/CloudbedsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudbedstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbedsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
